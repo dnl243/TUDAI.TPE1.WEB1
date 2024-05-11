2613f019de540e12ce6c05de3df36e70de7a4a09
dnl243 <dnl243@hotmail.com.ar>
2024-05-11 00:48:45 -0300

    responsive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 "use strict";
-
-const menuIcono = document.querySelector("#menuIcono");
-menuIcono.addEventListener("click", desplegarMenu);
-
+// icono menu
+const menuIcono = document.querySelector(".menuIcono");
 const menuDesplegable = document.querySelector(".menuDesplegable");
 
-function desplegarMenu(){
+
+function desplegar(){
   menuDesplegable.classList.toggle("oculto")
-  menuIcono.classList.toggle("bi-list");
-  menuIcono.classList.toggle("bi-x");
-}
+  menuIcono.classList.toggle("bi-list");//ícono "hamburguesa"
+  menuIcono.classList.toggle("bi-x");//ícono "x"
+};
+menuIcono.addEventListener("click", desplegar);
+
+//ocultar desplegable al seleccionar opción
+document.querySelectorAll(".menuDesplegable li a").forEach(e =>
+  e.addEventListener("click", desplegar)
+);
+
+//mostrar texto preguntas fracuentes
+function mostrar(txt){
+  document.querySelector(txt).classList.toggle("oculto");
+};
+
+document.querySelector(".btnPregFrec1").addEventListener("click", function() {
+  mostrar(".txtOculto1");
+  this.classList.toggle("bi-plus");
+  this.classList.toggle("bi-x");
+});
+document.querySelector(".btnPregFrec2").addEventListener("click", function() {
+  mostrar(".txtOculto2");
+  this.classList.toggle("bi-plus");
+  this.classList.toggle("bi-x");
+});
