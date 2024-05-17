353d0de8de7534c5a7e47951d8118865fddf4927
dnl243 <dnl243@hotmail.com.ar>
2024-05-17 12:01:58 -0300

    detalles

--- js/scriptFav.js
+++ js/scriptFav.js
@@ -1,14 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  
-  //capturo img
-  let imgFav = document.querySelector(".imgFav img");
 
   //capturo pelis de la lista
   document.querySelectorAll(".lista ul li").forEach((e) => 
     e.addEventListener("mouseover", () => 
-      imgFav.src = e.id
+      document.querySelector(".imgFav img").src = e.id
     )
   );
 
